42febc0675edd6b9535d9e1d3993da810e2551e4
RobzLegz <robzlegz@gmail.com>
2021-10-07 04:14:06 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Karjera man</title>
+      </Head>
+
+      <h1>Aplikācija</h1>
+    </div>
+  )
+}
