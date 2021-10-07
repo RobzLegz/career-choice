0c8d5471c836e6d5ff620e7f0ee48dfdc00e5fbf
RobzLegz <robzlegz@gmail.com>
2021-10-07 23:02:12 +0100

    css update

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -7,7 +7,7 @@ function Navigation() {
             <Link href="/">
                 <h2 className="cursor-pointer">Karjera man</h2>
             </Link>
-            <ul className="flex w-40 items-center justify-between">
+            <ul className="nav__links">
                 <li className="nav__link"><Link href="/">Sākums</Link></li>
                 <li className="nav__link"><Link href="/find">Aptauja</Link></li>
             </ul>
