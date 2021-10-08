1830473aa0d1ca8a85671e3d5c67161937d1a828
RobzLegz <robzlegz@gmail.com>
2021-10-08 23:06:03 +0100

    bob

---
+++ src/data/proffessions.ts
@@ -0,0 +1,55 @@
+const categories = [
+    {
+        name: "Sports",
+        stage: 1,
+        info: ""
+    },
+    {
+        name: "Māksla",
+        stage: 1,
+        info: ""
+    },
+    {
+        name: "Matemātika",
+        stage: 1,
+        info: ""
+    },
+    {
+        name: "Valodas",
+        stage: 1,
+        info: ""
+    },
+    {
+        name: "Mājturība",
+        stage: 1,
+        info: "Praktiska darbošanās"
+    },
+    {
+        name: "Datorika",
+        stage: 1,
+        info: "Darbs ar datoru"
+    },
+    {
+        name: "Kulinārija",
+        stage: 1,
+        info: "Darbs ar ēdienu"
+    },
+    {
+        name: "Zooloģija",
+        stage: 1,
+        info: "Darbs ar dzīvniekiem"
+    },
+    {
+        name: "",
+        stage: 1,
+        info: ""
+    },
+];
+
+const proffessions = [
+    {
+
+    },
+];
+
+export {categories, proffessions};
