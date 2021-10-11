e1e10e6bc52db94a70a8dec2f310cbab6d306fae
Laptops16 <92031986+Laptops16@users.noreply.github.com>
2021-10-11 19:57:44 +0300

    Update languages.ts

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -93,18 +93,18 @@ const languages = {
         global: {
             navigation: {
                 links: {
-                    link1: "Sākums",
-                    link2: "Aptauja"
+                    link1: "Home",
+                    link2: "Survey"
                 }
             },
             footer: {
                 pages: {
-                    heading: "Lapas",
-                    option1: "Sākums",
-                    option2: "Aptauja"
+                    heading: "Pages",
+                    option1: "Home",
+                    option2: "Survey"
                 },
                 media: {
-                    heading: "Sociālie tīkli",
+                    heading: "Social Network",
                     option1: "Instagram",
                     option2: "github"
                 }
@@ -112,52 +112,52 @@ const languages = {
         },
         home: {
             head: {
-                title: "Karjera man | Sākums"
+                title: "Karjera man | Home"
             },
             header: {
                 heading: "Karjera man",
-                subHeading: "Atrodi sev piemērotākās profesijas un karjeras iespējas"
+                subHeading: "Find the most suitable profession and career opportunities for you"
             },
             cards: {
                 card1: {
-                    heading: "Kas ir karjera?",
-                    text: "Karjera ir cilvēka mērķtiecīga darbība savu kompetenču (spēju, prasmju, zināšanu) pilnveidei un izpausmei mūža garumā. Karjeras veidošana ir nepārtraukts process, kura gaitā cilvēks izmanto informāciju par sevi un apkārtējo pasauli, analizē un pielieto to, lai izvēlētos savu nodarbošanās jomu un pēc tam jau konkrētu profesiju. Cilvēks pats piedalās savas dzīves (karjeras) veidošanā un plānošanā."
+                    heading: "What is career?", 
+                    text: "Career is man porposeful activity their competencies (abilities, skills, knowledge) for devlopment and expression throughout life. Career building is a continuous process in which a person usesinformation about himself and the world around him, analyzes and applies it to choose his or her field of occupation and then already a particular profession. Man himself participates in building and planning his own life (career)."
                 },
                 card2: {
-                    heading: "Filtrācija",
-                    text: "Mēs Jums uzdosim pāris jautājumus saistībā ar karjeru, un pēc atbilžu rezultātiem ar filtrācijas algoritma palīdzību atradīsim jums vispiemērotākās profesijas.",
-                    button: "Veikt aptauju"
+                    heading: "Filtration",
+                    text: "We will ask you a few questions related to career, and based on the results of the answers, we will use the filtring algorithm to find the most suitable professions for you.",
+                    button: "Survey"
                 },
                 card3: {
-                    heading: "Veiciet aptauju, lai atrastu sev piemērotāko profesiju",
-                    button: "Aptauja"
+                    heading: "Do a survey, to find the most suitable profession for yourself",
+                    button: "Survey"
                 },
                 card4: {
-                    heading: "Drošība",
-                    button: "Aptauja",
-                    textPart1: "Jūsu dati netiks saglabāti, vai nodoti reklāmdevējiem, mūsu lapas mērķis ir izglītot bērnus un jauniešus par darba iespējām, kā arī piedāvāt atbilstošākās profesijas nāktonei. Mūsu aplikācijas",
-                    link: "kods",
-                    textPart2: "ir publiski pieejams un visiem redzams."
+                    heading: "Security",
+                    button: "Survey",
+                    textPart1: "Your data will not be stored or passed on to advertisers, our page aims to educate children and teens about job opportunities, and to offer the right profession for the future. Mūsu aplikācijas",
+                    link: "code",
+                    textPart2: "Is publicly available and visible to everyone."
                 },
             }
         },
         find: {
             head: {
-                title: "Karjera man | Aptauja"
+                title: "Carrer for me | Survey"
             },
             header: {
-                heading: "Izvēlieties lietas",
-                positionHeading: "Jums atlasītās profesijas"
+                heading: "Choose what you like to do",
+                positionHeading: "Your selected professions"
             },
             buttonContainer: {
-                backButton: "Atpakaļ",
-                forwardButton: "Tālāk",
-                finishButton: "Rezultāti",
+                backButton: "Back",
+                forwardButton: "Next",
+                finishButton: "Results",
             },
             position: {
-                salaryText: "Vidējā alga",
-                loadMore: "Ielādēt vēl",
-                accuracyText: "atbilstība"
+                salaryText: "Avarge salary",
+                loadMore: "Load more",
+                accuracyText: "Conformity"
             }
         }
     },
