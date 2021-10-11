021575222a59197ffad067343378713bdfc12a3b
RobzLegz <robzlegz@gmail.com>
2021-10-11 22:54:43 +0100

    lang changing option start

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { useSelector } from 'react-redux';
 import { languageData } from '../../redux/slices/languageSlice';
+import { supportedLanguages } from '../../data/languages';
 
 function Navigation() {
     const languageInfo = useSelector(languageData);
+    const [languageChangerActive, setLanguageChangerActive] = useState(false);
 
     return (
         <nav>
@@ -14,6 +16,16 @@ function Navigation() {
             <ul className="nav__links">
                 <li className="nav__link"><Link href="/">{languageInfo.text.global.navigation.links.link1}</Link></li>
                 <li className="nav__link"><Link href="/find">{languageInfo.text.global.navigation.links.link2}</Link></li>
+                <li className="language__changer">
+                    {
+                        supportedLanguages.map((language) => (
+                            <div className="language__container">
+                                <img src={language.flag} alt={`${language.language} flag`} />
+                                <p>{language.short}</p>
+                            </div>
+                        ))
+                    }
+                </li>
             </ul>
         </nav>
     )
