0a66b28756689a41c05a623b92aed13b946b60ad
RobzLegz <robzlegz@gmail.com>
2021-10-12 05:06:48 +0100

    changing languages

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
-import { useSelector } from 'react-redux';
-import { languageData } from '../../redux/slices/languageSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeLanguage, languageData } from '../../redux/slices/languageSlice';
 import { supportedLanguages } from '../../data/languages';
 
 function Navigation() {
+    const dispatch = useDispatch();
     const languageInfo = useSelector(languageData);
     const [languageChangerActive, setLanguageChangerActive] = useState(false);
+ 
+    const changeLang = (lang: string) => {
+        dispatch(changeLanguage(lang));
+        setLanguageChangerActive(false);
+    }
 
     return (
         <nav>
@@ -16,14 +22,14 @@ function Navigation() {
             <ul className="nav__links">
                 <li className="nav__link"><Link href="/">{languageInfo.text.global.navigation.links.link1}</Link></li>
                 <li className="nav__link"><Link href="/find">{languageInfo.text.global.navigation.links.link2}</Link></li>
-                <li className="ml-5 flex items-center h-full">
+                <li className="ml-5 flex items-center h-full pt-1">
                     {!languageChangerActive ? (
                         <div className="w-16 h-5" onClick={() => setLanguageChangerActive(true)}>
                             {
                                 supportedLanguages.map((l) => {
                                     if(languageInfo.lang === l.short){
                                         return (
-                                            <div className="w-full h-full flex items-center justify-between">
+                                            <div className="w-full h-full flex items-center justify-between cursor-pointer">
                                                 <img className="w-10 h-full" src={l.flag} alt={`${l.language} flag`} />
                                                 <p className="ml-1">{l.short}</p>
                                             </div>
@@ -36,7 +42,7 @@ function Navigation() {
                         <>
                             {
                                 supportedLanguages.map((language) => (
-                                    <div className="h-5 flex ml-3 items-center">
+                                    <div className="h-5 flex ml-3 items-center cursor-pointer" onClick={() => changeLang(language.short)}>
                                         <img className="w-10 h-full" src={language.flag} alt={`${language.language} flag`} />
                                         <p className="ml-1">{language.short}</p>
                                     </div>
